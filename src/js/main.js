ec3b05b3f5c82cd9e8f035d3bcb0799f929ff7e8
hdertgaming <40216616+Hdertgaming@users.noreply.github.com>
2022-07-19 02:13:30 +1200

    added alert, account history

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 
 import Modal from "bootstrap/js/dist/modal";
 
-// import "bootstrap/js/dist/alert";
+import "bootstrap/js/dist/alert";
 import "bootstrap/js/dist/button";
 // import "bootstrap/js/dist/carousel";
 // import "bootstrap/js/dist/collapse";
@@ -19,10 +19,39 @@ const bananojs = require("@bananocoin/bananojs");
 bananojs.setBananodeApiUrl("https://kaliumapi.appditto.com/api");
 
 async function formHandler() {
-  let [account, balance] = await getAccountDetails();
+  let [account, banano, raw, pending, history] = await getAccountDetails();
   document.getElementById("infoModalHeaderH5").innerHTML = account;
-  document.getElementById("infoModalBody").innerHTML = "<p>" + balance + "</p>";
+  document.getElementById("infoModalBody").innerHTML =
+    "<p>" +
+    banano +
+    " ban</p>\n" +
+    "<small>+" +
+    raw +
+    " raw</small>\n" +
+    "<p>" +
+    pending +
+    " ban pending</p>" +
+    historyGenerator(history);
+
   new Modal(document.getElementById("infoModal")).show();
+}
+
+function historyGenerator(history) {
+  let string = "";
+  for (const [_, transaction] of Object.entries(history.history)) {
+    string +=
+      "<div class='card mb-2 bg-dark " +
+      (transaction.type == "send" ? "sent" : "received") +
+      "'><div class='card-body text-truncate'>" +
+      (transaction.type == "send" ? "Sent " : "Received ") +
+      bananojs.getBananoPartsFromRaw(transaction.amount).banano +
+      " ban " +
+      (transaction.type == "send" ? "to" : "from") +
+      ": " +
+      transaction.account +
+      "</div></div>\n";
+  }
+  return string;
 }
 
 document.getElementById("submitButton").onclick = function () {
@@ -31,10 +60,12 @@ document.getElementById("submitButton").onclick = function () {
 
 async function getAccountDetails() {
   let account = document.getElementById("textAreaInput").value;
-  let balance =
-    Math.round(
-      (await bananojs.getAccountBalanceRaw(account)) /
-        1000000000000000000000000000
-    ) / 100;
-  return [account, balance];
+  let accountBalance = await bananojs.getAccountBalanceAndPendingRaw(account);
+  let balance = accountBalance.balance;
+  let pending = accountBalance.pending;
+  let bananoParts = bananojs.getBananoPartsFromRaw(balance);
+  [banano, raw] = [bananoParts.banano, bananoParts.raw];
+  let bananoPending = bananojs.getBananoPartsFromRaw(pending).banano;
+  let history = await bananojs.getAccountHistory(account, 100);
+  return [account, banano, raw, bananoPending, history];
 }
